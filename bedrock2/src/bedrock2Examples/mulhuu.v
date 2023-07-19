e8c0f5a1eb36cb1c5dfe8e240a99ca9d5cf0bd80
Bill Harris <bll.hrris@gmail.com>
2023-07-18 22:40:54 -0700

    adds source file

---
+++ bedrock2/src/bedrock2Examples/mulhuu.v
@@ -0,0 +1,6 @@
+Require Import bedrock2.NotationsCustomEntry.
+
+Import Syntax BinInt String List.ListNotations.
+Local Open Scope string_scope.
+Local Open Scope Z_scope.
+Local Open Scope list_scope.

b0c2526cd6692a24333feed78f2acec610d6de1f
Bill Harris <bll.hrris@gmail.com>
2023-07-18 23:10:06 -0700

    adds mulhuu's spec

--- bedrock2/src/bedrock2Examples/mulhuu.v
+++ bedrock2/src/bedrock2Examples/mulhuu.v
@@ -1,15 +1,20 @@
 (* Implementation of mulhuu by Andres Erbsen. *)
 
 Require Import bedrock2.NotationsCustomEntry.
+Require Import coqutil.Macros.ident_to_string.
+From bedrock2 Require Import WeakestPrecondition ProgramLogic BasicC64Semantics.
+Require Import bedrock2.ZnWords.
 
 Import Syntax BinInt String List.ListNotations.
+Import coqutil.Word.Interface.
+
 Local Open Scope string_scope.
 Local Open Scope Z_scope.
 Local Open Scope list_scope.
-Require Import coqutil.Macros.ident_to_string.
 
 Local Notation "x += e" := (cmd.set (ident_to_string! x) (expr.op bopname.add (ident_to_string! x) e)) (in custom bedrock_cmd at level 0, x ident, e custom bedrock_expr, only parsing).
 
+(* Return the high word of the integer multiplication a * b. *)
 Definition mulhuu :=
   func! (a, b) ~> r2W {
       W = $4; (* TODO: translate sizeof(uintptr_t)? *)
@@ -27,4 +32,14 @@ Definition mulhuu :=
       s0 = r0 + (sW << W);
       c2W = s0 < r0;
       r2W += c2W
+    }.
+
+(* mulhuu's functional spec, as commented above. Memory and trace are
+   unchanged. *)
+Local Instance spec_of_add_with_carry : spec_of "add_with_carry" :=
+  fnspec! "mulhuu" a b ~> r2W,
+    { requires t m := True;
+      ensures T M :=
+        M = m /\ T = t /\
+          word.unsigned r2W = (word.unsigned a * word.unsigned b) / 2^64
     }.
